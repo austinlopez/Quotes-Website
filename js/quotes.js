7755df7adb1bc05471cf3a18a248b4848d845ca0
Austin Lopez <lopeza1857@aguafria.org>
2017-09-08 09:19:32 -0700

    9/8 Initial

---
+++ js/quotes.js
@@ -0,0 +1,19 @@
+/*Directions:
+1. Find 10 of your favorite quotes
+2. Make an array containing the quotes
+3. Have a webpage get one quote at random and display it
+*/
+
+
+var quoteArea = dicument.getElementById("htmlQuoteArea");
+
+
+var quotes = [
+  "\"Don\'t cry because it\'s over, smile because it happened.\"",
+  "Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.",
+]
+
+var quotesAuthor = [
+  "Dr. Suess",
+  "Albert Einstein",
+]
